edb46c87747f68876252914ce0b102868c5a8ca8
Ali <ali@coles-air.lan>
2022-07-11 16:03:30 -0500

    add app first_jsx to repo

---
+++ react_fund/first_jsx/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './components/main'
+import List from './components/list'
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+      <List />
+    </div>
+  );
+}
+
+export default App;
