163adb4b75fd3ea8dadd53c7b0ffe53352e47fda
Ali <ali@coles-air.lan>
2022-07-15 15:36:27 -0500

    add app prop_it2  to repo

--- react_fund/prop_it/src/components/PersonCard.js
+++ react_fund/prop_it/src/components/PersonCard.js
@@ -1,12 +1,17 @@
+import {useState} from 'react';
 const PersonCard = (props) => {
     const {person} = props;
+
+    const [state, setState] = useState(person.age)
+    
 
     return(
         <div>
             <h1>First Name: {person.firstName}</h1>
             <h1>Last Name: {person.lastName}</h1>
-            <h2>Age: {person.age}</h2>
+            <h2>Age: {state}</h2>
             <h2>Hair Color: {person.hairColor}</h2>
+            <button onClick= {()=>setState(state+1)}>Birthday Button {person.firstName}</button>
         </div>
     )
 }
